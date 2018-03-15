90e5ef48f1b9c727ad44f79a9c1b7f390da7195a
Trent <trent.stenoien@gmail.com>
2018-03-15 00:53:49 -0500

    Near complete overhaul, but everything is more manageable now.

--- js/app.js
+++ js/app.js
@@ -1,111 +1,175 @@
-$(window).ready(()=> {
+// Ensure a value is both present and valid; returns object
+const testValue = (value, regex) => {
+  let test = {};
+  test.hasValue = value !== '';
+  if (regex) test.valid = regex.test(value);
+  return test;
+}
 
-  // Hide fields made optional with JavaScript, focus name input, prevent button refresh while in development
-  $('#other-title').hide();
-  $('#colors-js-puns').hide();
-  $('#credit-card').hide();
-  $('#paypal').hide();
-  $('#bitcoin').hide();
-  $('#name').focus();
-  $('button').click(e => e.preventDefault());
-
-  // Attach total price display to 'activities' div
-  (() => {
-    const $h4 = $('<h4>Total Price: $ <span class="price">0</span></h4>');
-    $('.activities').append($h4);
-  })()
-
-  // When focus moves away from 'name', check the value and alert the user if corrections are needed
-  $('#name').focusout( e => {
-    const name = $(e.target).val();
-    const regex = /^[a-z]+\s+[a-z]+\s*$/i;
-    if (name === '') console.log('Error: Please enter your first and last name');
-    else if (!regex.test(name)) console.log('Error: Please enter first and last name. Names can only contain letters');
+const loadColors = design => {
+  const designIsPuns = design === 'js puns';
+  $('#color').children().each((index, tshirtColor) => {
+    const colorIsPuns = $(tshirtColor).text().indexOf('JS Puns') >= 0;
+    const match = (colorIsPuns === designIsPuns) || ($(tshirtColor).val() === '');
+    ( match ? $(tshirtColor).show(): $(tshirtColor).hide() );
   });
+  $('#color').val('');
+  $('#colors-js-puns').fadeIn(300);
+}
 
-  // When focus moves away from 'email', check the value and alert the user if corrections are needed
-  $('#mail').focusout( e => {
-    const email = $(e.target).val();
-    const regex = /^[\w\.]+@[a-z]+\.[a-z]+$/i;
-    if (email === '') console.log('Error: Please enter your email address');
-    else if (!regex.test(email)) console.log('Error: Please enter a valid email address with the format of "my_email@google.com". Words can only use letters, numbers, periods, or underscores.');
-  });
+const configureSchedule = $element => {
+  console.log($element);
 
-  // When job dropdown value changes, check for value 'other' and show .other-title input if needed
-  $('#title').change( e => {
-    const $otherTitle = $('#other-title');
-    const showOtherTitle = $(e.target).val() === 'other';
-    ( showOtherTitle ? $otherTitle.fadeIn(300) : $otherTitle.fadeOut(300) );
-  });
+  // calculateTotalPrice
+  // toggleActivities
+}
 
-  // Function compares theme against colors to find which to display, resets '#color' value, then displays '#color dropdown'
-  const showColors = theme => {
-    const themeIsPuns = theme === 'js puns';
+const loadPaymentMethod = $paymentMenu => {
+  $paymentMenu.parent().children('div').each((index, element) => $(element).hide());
+  if ($paymentMenu.val() === 'credit card') $('#credit-card').fadeIn(300);
+  else if ($paymentMenu.val() === 'paypal') $('#paypal').fadeIn(300);
+  else if ($paymentMenu.val() === 'bitcoin') $('#bitcoin').fadeIn(300);
+}
 
-    $('#color').children().each((index, tshirtColor) => {
-      const colorIsPuns = $(tshirtColor).text().indexOf('JS Puns') >= 0;
-      const match = colorIsPuns === themeIsPuns;
-      ( match ? $(tshirtColor).show(): $(tshirtColor).hide() );
-    });
 
-    $('#color').val('');
-    $('#colors-js-puns').fadeIn(300);
+
+
+
+
+
+const errorHandler = (element, turnOn, message) => {
+  const $element = $(element);
+  if ($element.next().attr('class') === 'error-message') $element.next().remove();
+
+  if (turnOn) {
+    $element.addClass('error');
+    const $label = $(`<label class="error-message"></label>`);
+    if (message) $label.text(message);
+    $element.after($label);
+  } else {
+    $element.removeClass('error');
   }
+}
 
-  // On selection of tshirt design, show dropdown with only the colors in that style or hide it altogether if no style chosen
-  $('#design').change( e => {
-    const designTheme = $(e.target).val();
-    ( designTheme !== 'Select Theme' ? showColors(designTheme) : $('#colors-js-puns').fadeOut(300) );
-  });
 
-  // Take an activity string, divide it into information and return as object
-  const findActivityInfo = activity => {
-    const regexFull = /([a-z]+)\s([1-9]+[ap]m)-([1-9]+[ap]m),\s\$([0-9]{3})$/i;
-    const regexMainConf = /\$([0-9]{3})$/;
 
-    let day, start, end, price;
-    let info = activity.match(regexFull);
-    if (info === null) price = activity.match(regexMainConf)[1];
-    else [, day, start, end, price] = info;
 
-    price = Number(price);
 
-    return {day, start, end, price};
+// Takes the name field and tests its value to either throw error or
+const validateName = $nameField => {
+  const regex = /^[a-z]+\s+[a-z]+\s*$/i;
+  const test = testValue($nameField.val(), regex);
+  if (!test.hasValue) errorHandler($nameField, true, 'Enter your first and last name above');
+  else if (!test.valid) errorHandler($nameField, true, 'Enter your first and last name, letters only with a space');
+  else errorHandler($nameField, false);
+}
+
+// Takes the email field and tests its value to either throw error or
+const validateEmail = $emailField => {
+  const regex = /^[a-z0-9\._]+@[a-z]+\.[a-z]+$/i;
+  const test = testValue($emailField.val(), regex);
+  if (!test.hasValue) errorHandler($emailField, true, 'Enter your email address above');
+  else if (!test.valid) errorHandler($emailField, true, 'Email format: someone@example.com');
+  else errorHandler($emailField, false);
+}
+
+const validateTitle = $titleMenu => {
+  const test = testValue($titleMenu.val());
+  if (!test.hasValue) errorHandler($titleMenu, true, 'Select your job title');
+  else errorHandler($titleMenu, false);
+  if ($titleMenu.val() === 'other') $('#other-title').fadeIn(300);
+  else $('#other-title').hide();
+}
+
+const validateOtherTitle = $otherTitleField => {
+  const test = testValue($otherTitleField.val());
+  if (!test.hasValue) errorHandler($otherTitleField, true, 'Enter your title above');
+  else errorHandler($otherTitleField, false);
+}
+
+const validateSize = $sizeMenu => {
+  const test = testValue($sizeMenu.val());
+  if (!test.hasValue) errorHandler($sizeMenu, true);
+  else errorHandler($sizeMenu, false);
+}
+
+const validateDesign = $designMenu => {
+  const test = testValue($designMenu.val());
+  if (!test.hasValue) {
+    errorHandler($designMenu, true);
+    $('#colors-js-puns').hide();
+  } else {
+    errorHandler($designMenu, false);
+    loadColors($designMenu.val());
   }
+}
+
+const validateColor = $colorMenu => {
+  const test = testValue($colorMenu.val());
+  if (!test.hasValue) errorHandler($colorMenu, true);
+  else errorHandler($colorMenu, false);
+}
+
+const validateActivities = $activitiesDiv => {
+  console.log($activitiesDiv);
+  // if any are checked, remove error and move on
+  // else create error under legend
+}
+
+const validatePayment = ($paymentMenu, newMethod) => {
+  const test = testValue($paymentMenu.val());
+  if (!test.hasValue) errorHandler($paymentMenu, true, 'Select your payment method above');
+  else errorHandler($paymentMenu, false);
+  if (newMethod) loadPaymentMethod($paymentMenu);
+}
 
 
-  // On box check, prohibit timing conflicts, and calculate total price
-  $('.activities label input').change( e => {
-    const activitySelected = $(e.target).parent().text();
-    const activityInfo = findActivityInfo(activitySelected);
-    const currentPrice = Number($('.price').text());
-    const checked = e.target.checked;
 
-    if (checked) $('.price').text( currentPrice + activityInfo.price );
-    else $('.price').text( currentPrice - activityInfo.price );
-  });
 
-  // On selection of payment method, show proper payment information
-  $('#payment').change( e => {
-    $(e.target).parent().children('div').hide();
 
-    const paymentMethod = $(e.target).val();
-    if (paymentMethod === 'bitcoin') $('#bitcoin').show();
-    else if (paymentMethod === 'paypal') $('#paypal').show();
-    else if (paymentMethod === 'credit card') $('#credit-card').show();
-  });
 
-  // When focus moves away from any credit card field, check the value and alert the user if corrections are needed
-  $('#credit-card').focusout(e => {
-    const $field = $(e.target);
-    const num = $field.val();
-    const regex = (() => {
-      if ($field.attr('id') === 'cc-num' ) return /^\d{13,16}$/;
-      else if ($field.attr('id') === 'zip' ) return /^\d{5}$/;
-      else if ($field.attr('id') === 'cvv' ) return /^\d{3}$/;
-      else return null;
-    })()
 
-    if (regex && !regex.test(num)) console.log($field.attr('id'));
-  });
-});
+
+// This function validates every form element
+const validateForm = () => {
+
+  validateName($('#name'));
+  validateEmail($('#mail'));
+  validateTitle($('#title'));
+  if ($('#title').val() === 'other') validateOtherTitle($('#other-title'));
+
+  validateSize($('#size'));
+  validateDesign($('#design'));
+  validateColor($('#color'));
+
+  validateActivities($('.activities'));
+
+  validatePayment($('#payment'));
+  if ($('#payment').val() === 'credit card') validateCC($('#credit-card'));
+
+}
+
+/****************** APP ******************/
+
+$('#other-title').hide();
+$('#colors-js-puns').hide();
+$('#credit-card').hide();
+$('#paypal').hide();
+$('#bitcoin').hide();
+
+$('#name').focus();
+
+
+
+$('#name').focusout(e => { validateName($(e.target)) });
+$('#mail').focusout(e => { validateEmail($(e.target)) });
+$('#title').change(e => { validateTitle($(e.target)) });
+$('#other-title').focusout(e => { validateOtherTitle($(e.target)) });
+
+$('#size').change(e => { validateSize($(e.target)) });
+$('#design').change(e => { validateDesign($(e.target)) });
+$('#colors-js-puns').change(e => { validateColor($(e.target)) });
+
+$('.activities input').change(e => { configureSchedule($(e.target)) });
+
+$('#payment').change(e => { validatePayment($(e.target), true) });
