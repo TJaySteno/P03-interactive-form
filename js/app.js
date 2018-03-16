fc654abce3425556c22df479a39335a624c54fed
Trent <trent.stenoien@gmail.com>
2018-03-16 11:39:27 -0500

    Activities validate & disallow conflicts. Fixed a bug in CC validation.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+/**************************************
+GENERAL PURPOSE
+**************************************/
 // Ensure a value is both present and valid; returns object
 const testValue = (value, regex) => {
   let test = {};
@@ -6,6 +9,7 @@ const testValue = (value, regex) => {
   return test;
 }
 
+// Determine proper colors to display based on design, then fade in '#color' menu
 const loadColors = design => {
   const designIsPuns = design === 'js puns';
   $('#color').children().each((i, tshirtColor) => {
@@ -17,11 +21,62 @@ const loadColors = design => {
   $('#colors-js-puns').fadeIn(300);
 }
 
-const configureSchedule = $element => {
-  console.log($element);
+// Divide class string into the proper info; returns an object
+const getClassInfo = classString => {
+  const dayAndTime = classString.match(/([a-z]+)\s([1-9]+[ap]m)/i);
+  let day, time;
+  if (dayAndTime !== null) [, day, time] = dayAndTime;
+  const price = Number(classString.match(/\$([0-9]{3})$/)[1]);
+  return {day, time, price};
+}
 
-  // calculateTotalPrice
-  // toggleActivities
+//
+const formatClass = (classNode, allow) => {
+  if (!allow) {
+    $(classNode).css('color', 'grey');
+    $(classNode.firstChild).css('visibility', 'hidden');
+  } else {
+    $(classNode).css('color', 'black');
+    $(classNode.firstChild).css('visibility', 'visible');
+  }
+}
+
+// Show conflicting activities as available or unavailable depending whether user selects and unselects a class
+const toggleActivities = (classInfo, toggle) => {
+  const classes = $('.activities').children();
+  if (classInfo.day === 'Tuesday' && classInfo.time === '9am') {
+    if (toggle === 'allow') {
+      allowClass(classes[2], true);
+      allowClass(classes[4], true);
+    } else if (classes[2].firstChild.checked) allowClass(classes[4], false);
+    else allowClass(classes[2], false);
+  } else if (classInfo.day === 'Tuesday' && classInfo.time === '1pm') {
+    if (toggle === 'allow') {
+      allowClass(classes[3], true);
+      allowClass(classes[5], true);
+    } else if (classes[3].firstChild.checked) allowClass(classes[5], false);
+    else allowClass(classes[3], false);
+  }
+}
+
+// Add or subtract an activity's price from the total
+const calculateTotalPrice = (activityPrice, operation) => {
+  const currentPrice = Number($('.price').text());
+
+  if (operation === '+') $('.price').text( currentPrice + activityPrice );
+  else $('.price').text( currentPrice - activityPrice );
+}
+
+// Upon class selection, calculate price change and disallow activities at conflicting times
+const configureSchedule = $element => {
+  const classInfo = getClassInfo($element.parent().text());
+  if ($element.prop('checked')) {
+    toggleActivities(classInfo, 'disallow');
+    calculateTotalPrice(classInfo.price, '+');
+  } else {
+    toggleActivities(classInfo, 'allow');
+    calculateTotalPrice(classInfo.price, '-');
+  }
 }
 
 // Reset payment display, then load the proper div
@@ -35,8 +90,8 @@ const loadPaymentMethod = $paymentMenu => {
   else if ($paymentMenu.val() === 'bitcoin') $('#bitcoin').fadeIn(300);
 }
 
-const errorHandler = (element, turnOn, message) => {
-  const $element = $(element);
+// Assigns to removes an error from a given element
+const errorHandler = ($element, turnOn, message) => {
   if ($element.next().attr('class') === 'error-message') $element.next().remove();
 
   if (turnOn) {
@@ -46,12 +101,16 @@ const errorHandler = (element, turnOn, message) => {
       $label.text(message);
       $element.after($label);
     }
-  } else {
-    $element.removeClass('error');
-  }
+  } else $element.removeClass('error');
 }
 
-// Takes the name field and tests its value to either throw error or
+
+
+/**************************************
+FORM VALIDATION
+**************************************/
+
+// Tests 'name' field's value, throws error on a bad match
 const validateName = $nameField => {
   const regex = /^[a-z]+\s+[a-z]+\s*$/i;
   const test = testValue($nameField.val(), regex);
@@ -60,7 +119,7 @@ const validateName = $nameField => {
   else errorHandler($nameField, false);
 }
 
-// Takes the email field and tests its value to either throw error or
+// Tests 'email' field's value, throws error on a bad match
 const validateEmail = $emailField => {
   const regex = /^[a-z0-9\._]+@[a-z]+\.[a-z]+$/i;
   const test = testValue($emailField.val(), regex);
@@ -69,26 +128,33 @@ const validateEmail = $emailField => {
   else errorHandler($emailField, false);
 }
 
+// Tests 'title' menu's value, throws error if no value given. Loads 'other-title' when needed.
 const validateTitle = $titleMenu => {
   const test = testValue($titleMenu.val());
   if (!test.hasValue) errorHandler($titleMenu, true, 'Select your job title');
   else errorHandler($titleMenu, false);
   if ($titleMenu.val() === 'other') $('#other-title').fadeIn(300);
-  else $('#other-title').hide();
+  else {
+    errorHandler($('#other-title'), false);
+    $('#other-title').hide();
+  }
 }
 
+// Tests 'other-title' field's value, throws error if no value given
 const validateOtherTitle = $otherTitleField => {
   const test = testValue($otherTitleField.val());
   if (!test.hasValue) errorHandler($otherTitleField, true, 'Enter your title above');
   else errorHandler($otherTitleField, false);
 }
 
+// Tests 'size' menu's value, throws error if no value given
 const validateSize = $sizeMenu => {
   const test = testValue($sizeMenu.val());
   if (!test.hasValue) errorHandler($sizeMenu, true);
   else errorHandler($sizeMenu, false);
 }
 
+// Tests 'design' menu's value, throws error if no value given. Loads 'color' when needed.
 const validateDesign = ($designMenu, newColors) => {
   const test = testValue($designMenu.val());
   if (!test.hasValue) {
@@ -100,18 +166,27 @@ const validateDesign = ($designMenu, newColors) => {
   }
 }
 
+// Tests 'color' menu's value, throws error if no value given
 const validateColor = $colorMenu => {
   const test = testValue($colorMenu.val());
   if (!test.hasValue) errorHandler($colorMenu, true);
   else errorHandler($colorMenu, false);
 }
 
+// Ensures at least one activity is selected, throws error if none have been chosen
 const validateActivities = $activitiesDiv => {
-  console.log($activitiesDiv);
-  // if any are checked, remove error and move on
-  // else create error under legend
+  let nothingChecked = true;
+  $activitiesDiv.children('label').each((i, label) => {
+    if (label.firstChild.checked) nothingChecked = false;
+  });
+  if (nothingChecked) {
+    $('.activities legend').addClass('error-message').text('Register for at least one activity');
+  } else {
+    $('.activities legend').removeClass('error-message').text('Register for Activities');
+  }
 }
 
+// Tests 'payment' menu's value, throws error if no value given. Loads proper payment method.
 const validatePayment = ($paymentMenu, newMethod) => {
   const test = testValue($paymentMenu.val());
   if (!test.hasValue) errorHandler($paymentMenu, true, 'Select your payment method above');
@@ -119,6 +194,7 @@ const validatePayment = ($paymentMenu, newMethod) => {
   if (newMethod) loadPaymentMethod($paymentMenu);
 }
 
+// Tests 'cc-num' field's value, throws error on a bad match
 const validateCCN = $ccnField => {
   const regex = /^\d{13,16}$/;
   const test = testValue($ccnField.val(), regex);
@@ -127,6 +203,7 @@ const validateCCN = $ccnField => {
   else errorHandler($ccnField, false);
 }
 
+// Tests 'zip' field's value, throws error on a bad match
 const validateZip = $zipField => {
   const regex = /^\d{5}$/;
   const test = testValue($zipField.val(), regex);
@@ -135,6 +212,7 @@ const validateZip = $zipField => {
   else errorHandler($zipField, false);
 }
 
+// Tests 'cvv' field's value, throws error on a bad match
 const validateCVV = $cvvField => {
   const regex = /^\d{3}$/;
   const test = testValue($cvvField.val(), regex);
@@ -143,6 +221,7 @@ const validateCVV = $cvvField => {
   else errorHandler($cvvField, false);
 }
 
+// Checks expiration date against today's date
 const validateExpiration = ($monthMenu, $yearMenu) => {
   const currentMonth = new Date().getMonth() + 1;
   const currentYear = new Date().getFullYear();
@@ -157,15 +236,16 @@ const validateExpiration = ($monthMenu, $yearMenu) => {
   }
 }
 
+// Runs a list of functions to ensure the credit card values are valid
 const validateCreditCard = $creditCardDiv => {
-  validateCCN($creditCardDiv.children('#cc-num'));
-  validateZip($creditCardDiv.children('#zip'));
-  validateCVV($creditCardDiv.children('#cvv'));
-  validateExpiration($creditCardDiv.children('#exp-month'),
-                     $creditCardDiv.children('#exp-year'));
+  validateCCN($creditCardDiv.find('#cc-num'));
+  validateZip($creditCardDiv.find('#zip'));
+  validateCVV($creditCardDiv.find('#cvv'));
+  validateExpiration($creditCardDiv.find('#exp-month'),
+                     $creditCardDiv.find('#exp-year'));
 }
 
-// This function validates every form element
+// Verifies info given is valid. If anything is missing it returns a user to the top, otherwise it will submit the form.
 const validateForm = e => {
   e.preventDefault()
 
@@ -185,14 +265,25 @@ const validateForm = e => {
   if ($('#payment').val() === 'credit card') validateCreditCard($('#credit-card'));
 
 
-  if ($('.error').length) {
+  if ($('.error').length || $('.error-message').length) {
     document.location.replace(`#`);
-    $($('legend')[0]).after('<label class="error-header">You seem to have missed something</label>');
-  } else $(this).unbind('validateForm').submit();
+    if (!$('.error-header').length) {
+      const $errorHeader = $('<label class="error-header">You seem to have missed something</label>');
+      $($('legend')[0]).after($errorHeader);
+    }
+  } else {
+    $('.error-header').remove();
+    $('button').unbind('validateForm').submit();
+  }
 }
 
-/****************** APP EXECUTION ******************/
 
+
+/**************************************
+APP EXECUTION
+**************************************/
+
+// Focus 'name', hide situational elements
 $('#name').focus();
 $('#other-title').hide();
 $('#colors-js-puns').hide();
@@ -200,8 +291,11 @@ $('#credit-card').hide();
 $('#paypal').hide();
 $('#bitcoin').hide();
 
+// Create price display
+const $h4 = $('<h4>Total Price: $ <span class="price">0</span></h4>');
+$('.activities').append($h4);
 
-
+// Event Listeners
 $('#name').focusout(e => { validateName($(e.target)) });
 $('#mail').focusout(e => { validateEmail($(e.target)) });
 $('#title').change(e => { validateTitle($(e.target)) });
@@ -217,5 +311,6 @@ $('#payment').change(e => { validatePayment($(e.target), true) });
 $('#cc-num').focusout(e => { validateCCN($(e.target)) });
 $('#zip').focusout(e => { validateZip($(e.target)) });
 $('#cvv').focusout(e => { validateCVV($(e.target)) });
+$('#exp-year').focusout(e => { validateExpiration($(e.target).parent().find('#exp-month'), $(e.target)) });
 
 $('button').click(e => { validateForm(e) });
